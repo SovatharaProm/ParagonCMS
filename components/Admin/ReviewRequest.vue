780f1ef6dca632e7a34b06c2c7fa87b4eb39292e
Sovathara Prom <tharax3000@gmail.com>
2024-07-09 15:44:53 +0700

    fix grid 1 for preview link

--- components/Admin/ReviewRequest.vue
+++ components/Admin/ReviewRequest.vue
@@ -3,7 +3,7 @@
     {{ changeRequest?.request_name || "Loading..." }}
   </h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-1 gap-6">
       <div>
         <p class="text-lg font-semibold mb-2">
           <span class="font-bold">Request Name:</span>
